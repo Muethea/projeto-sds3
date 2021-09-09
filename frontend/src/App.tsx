e1821a49f8694ba4f96eaac2ad490ac29dd0dd01
Fernando dos Santos Muethea <fmuethea4@gmail.com>
2021-09-09 15:23:44 +0200

    Static charts

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,20 +1,38 @@
+import BarChart from "components/BarChart";
 import DataTable from "components/DataTable";
+import DonutChart from "components/DonutChart";
 import Footer from "components/Footer";
 import NavBar from "components/NavBar";
-
 
 function App() {
   return (
     <>
-
       <NavBar />
       <div className="container">
-      <h1 className="text-primary">
-        Ola Mundo
-      </h1>
-      <DataTable />
-    </div>
-    <Footer />
+        <h1 className="text-primary py-3">
+          Dashbord de Vendas
+          <div className="row px-3">
+            <div className="col-sm-6">
+              <h5 className="text-center text-secondary">
+                Taxa de Sucesso (%)
+              </h5>
+              <BarChart />
+            </div>
+
+            <div className="col-sm-6">
+              <h5 className="text-center text-secondary">
+                Taxa de Sucesso (%)
+              </h5>
+              <DonutChart />
+            </div>
+          </div>
+        </h1>
+        <div className="py-3">
+          <h2 className="text-primary">Todas Vendas</h2>
+        </div>
+        <DataTable />
+      </div>
+      <Footer />
     </>
   );
 }
